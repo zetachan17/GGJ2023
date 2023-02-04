761722bd902ca0776a93716c73742ed45c0c05d9
YanYiHeng <zosanivan@gmail.com>
2023-02-04 16:25:48 -0500

    add the asset back

--- Assets/Scripts/Customer/Customer.cs
+++ Assets/Scripts/Customer/Customer.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Customer : MonoBehaviour
 {
     // GameObject _gameObject
-    Power _power;
+    private Power _power;
     private enum Power
     {
         Fire,
