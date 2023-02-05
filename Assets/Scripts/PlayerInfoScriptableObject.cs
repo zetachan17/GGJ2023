d1ce1492edeaf5c4d2f7f688b936224fb11d666e
Runze Zhu <z_runze@live.concordia.ca>
2023-02-04 19:07:58 -0500

    PlayerScriptableObject

---
+++ Assets/Scripts/PlayerInfoScriptableObject.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "PlayerScriptableObject", menuName = "Scriptable/PlayerInfo")]
+public class PlayerInfoScriptableObject : ScriptableObject
+{
+    [SerializeField]
+    private List<string> playerAttributesList;
+
+    [System.NonSerialized]
+    public UnityEvent<GameObject> addPlayerEvent;
+
+    private void OnEnable()
+    {
+        playerAttributesList = new List<string>();
+    }
+    
+    public void AddPlayer(string newName, int choseAnimal)
+    {
+        //GameObject playerObject = new GameObject(newName);
+        //PlayerAttributes player = playerObject.AddComponent<PlayerAttributes>();
+        //player.PlayerName = newName;
+        //player.ChosenAnimal = choseAnimal;
+        
+        playerAttributesList.Add(newName);
+        playerAttributesList.Add(choseAnimal.ToString());
+    }
+}
+
+
+
